2c81fb6c95c4e622e6b493add4d8317ff1a7b150
Jeffrey Roberts <jeffrey.roberts@my.simpson.edu>
2019-12-04 09:49:17 -0600

    Worked on Game

---
+++ Tiles/Tiled Preset Tilesets/enemies.tsx
@@ -0,0 +1,200 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="enemies" tilewidth="128" tileheight="128" tilecount="57" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/barnacle.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="18" y="22.6667" width="91.6667" height="105.667"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/barnacle_attack.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/barnacle_dead.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/bee.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/bee_dead.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/bee_move.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishBlue.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishBlue_dead.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishBlue_fall.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishGreen.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishGreen_dead.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishGreen_move.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishPink.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishPink_dead.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fishPink_move.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fly.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fly_dead.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/fly_move.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/frog.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/frog_dead.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/frog_move.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/ladybug.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/ladybug_fly.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/ladybug_move.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/mouse.png"/>
+  <animation>
+   <frame tileid="24" duration="350"/>
+   <frame tileid="26" duration="350"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/mouse_dead.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/mouse_move.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/saw.png"/>
+  <animation>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="29" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/saw_dead.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/saw_move.png"/>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="right_bound" value="4"/>
+  </properties>
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/sawHalf.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="7" y="71.3333" width="113.333" height="57"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="32" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/sawHalf_dead.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/sawHalf_move.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlock.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlock_dead.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlock_move.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlue.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlue_dead.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlue_hit.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeBlue_move.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeGreen.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeGreen_dead.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeGreen_hit.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimeGreen_move.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimePurple.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimePurple_dead.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimePurple_hit.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/slimePurple_move.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/snail.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/snail_move.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/snail_shell.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/wormGreen.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/wormGreen_dead.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/wormGreen_move.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/wormPink.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/wormPink_dead.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Enemies/wormPink_move.png"/>
+ </tile>
+</tileset>
---
+++ Tiles/Tiled Preset Tilesets/tiles.tsx
@@ -0,0 +1,250 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="128" tileheight="128" tilecount="72" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/bomb.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/bombWhite.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="hit_points" value="5"/>
+  </properties>
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCoin.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCoin_boxed.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCoin_disabled.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCoin_disabled_boxed.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCrate.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCrate_double.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCrate_single.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxCrate_warning.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxExplosive.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxExplosive_disabled.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxExplosive_used.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxItem.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxItem_boxed.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxItem_disabled.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/boxItem_disabled_boxed.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/brickBrown.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/brickGrey.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/bridgeA.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/bridgeB.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/bush.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/cactus.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/chain.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="47" y="0.333333" width="35.0417" height="127.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/doorClosed_mid.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/doorClosed_top.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/doorOpen_mid.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/doorOpen_top.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/fence.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/fenceBroken.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/grass.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/ladderMid.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/ladderTop.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lava.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lavaTop_high.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="8.5">
+    <polyline points="0,0 13.5,5.25 24.5,8.75 43,8.75 59.5,2.75 73.25,-5 88,-8.5 105.5,-8.5 114.25,-6.75 122.75,-3.75 127.75,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lavaTop_low.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="45.5">
+    <polygon points="0,0 11.875,5.25 19,7.375 25.75,8.25 42.125,8.625 55.25,4.375 71.25,-4.25 79.375,-6.5 87.25,-8.125 105.75,-8.5 113.625,-6.75 122.375,-3.75 127.875,0.625 128.125,82.25 -0.375,82.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/leverLeft.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/leverMid.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/leverRight.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lockBlue.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lockGreen.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lockRed.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/lockYellow.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/mushroomBrown.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/mushroomRed.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/plantPurple.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/rock.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/sign.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/signExit.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/signLeft.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/signRight.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/snow.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/spikes.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/spring.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/sprung.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchBlue.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchBlue_pressed.png"/>
+ </tile>
+ <tile id="57">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchGreen.png"/>
+ </tile>
+ <tile id="58">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchGreen_pressed.png"/>
+ </tile>
+ <tile id="59">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchRed.png"/>
+ </tile>
+ <tile id="60">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchRed_pressed.png"/>
+ </tile>
+ <tile id="61">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchYellow.png"/>
+ </tile>
+ <tile id="62">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/switchYellow_pressed.png"/>
+ </tile>
+ <tile id="63">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/torch1.png"/>
+  <animation>
+   <frame tileid="63" duration="270"/>
+   <frame tileid="64" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="64">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/torch2.png"/>
+ </tile>
+ <tile id="65">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/torchOff.png"/>
+ </tile>
+ <tile id="66">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/water.png"/>
+ </tile>
+ <tile id="67">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/waterTop_high.png"/>
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="8.75">
+    <polyline points="0,0 13.5,5.25 24.5,8.75 43,8.75 59.5,2.75 73.25,-5 88,-8.5 105.5,-8.5 114.25,-6.75 122.75,-3.75 127.75,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/waterTop_low.png"/>
+  <objectgroup draworder="index">
+   <object id="5" x="0.125" y="45.5">
+    <polygon points="0,0 11.875,5.25 19,7.375 25.75,8.25 42.125,8.625 55.25,4.375 71.25,-4.25 79.375,-6.5 87.25,-8.125 105.75,-8.5 113.625,-6.75 122.375,-3.75 127.875,0.625 128.125,82.25 -0.375,82.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/weight.png"/>
+ </tile>
+ <tile id="70">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/weightAttached.png"/>
+ </tile>
+ <tile id="71">
+  <image width="128" height="128" source="../../../CMSC 150 - Introduction to Programming/roberts-jeffrey-arcade-games-work/Lab 12 - Final Lab/Tiles/window.png"/>
+ </tile>
+</tileset>
